20c6628e5d97e170196e47425e5842929a6773ef
santhoshika <santhoshikasagi@gmail.com>
2023-12-14 12:49:32 +0530

    second commit

---
+++ Practice/src/Source/mynewclass.java
@@ -0,0 +1,14 @@
+package Source;
+
+public class mynewclass {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("this is my new project");
+		
+		
+
+	}
+
+}
